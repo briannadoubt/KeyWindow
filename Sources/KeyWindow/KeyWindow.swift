811b0b42431295843b1dcd6551c4a545516dd232
Brianna Doubt <bri@briannadoubt.com>
2021-10-22 16:06:08 -0700

    Move DeviceRotation into separate library

--- Sources/KeyWindow/KeyWindow.swift
+++ Sources/KeyWindow/KeyWindow.swift
@@ -34,21 +34,3 @@ public extension View {
         UIScreen.main.bounds
     }
 }
-
-public struct DeviceRotationViewModifier: ViewModifier {
-    public let action: (UIDeviceOrientation) -> Void
-
-    public func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
-    }
-}
-
-public extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(DeviceRotationViewModifier(action: action))
-    }
-}
