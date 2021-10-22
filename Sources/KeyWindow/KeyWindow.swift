5ec0ecb785f3143e9a6e767e4cb64f09ead4f413
Brianna Doubt <bri@briannadoubt.com>
2021-10-22 15:35:49 -0700

    Initial commit

--- Sources/KeyWindow/KeyWindow.swift
+++ Sources/KeyWindow/KeyWindow.swift
@@ -1,6 +1,54 @@
-public struct KeyWindow {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import SwiftUI
+
+public extension UIView {
+    static func keyWindow() -> UIWindow? {
+        return UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+    }
+}
+
+public extension View {
+
+    var keyWindowSafeAreaFrame: CGRect {
+        let safeAreaInsets = UIView.keyWindow()?.safeAreaInsets ?? .zero
+        let frame = keyWindowFrame.inset(by: safeAreaInsets)
+        return frame
+    }
+
+    var keyWindowTopSafeArea: CGFloat {
+        let safeAreaInsets = UIView.keyWindow()?.safeAreaInsets ?? .zero
+        return safeAreaInsets.top
+    }
+    
+    var keyWindowBottomSafeArea: CGFloat {
+        let safeAreaInsets = UIView.keyWindow()?.safeAreaInsets ?? .zero
+        return safeAreaInsets.bottom
+    }
+
+    var keyWindowFrame: CGRect {
+        UIScreen.main.bounds
+    }
+}
+
+public struct DeviceRotationViewModifier: ViewModifier {
+    public let action: (UIDeviceOrientation) -> Void
+
+    public func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+public extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
     }
 }
